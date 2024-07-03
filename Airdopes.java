d9ba60c3e9a5dba144285e4d45df7449a78cbb32
sinchanxworkz <shettysinchana856@gmail.com>
2024-07-03 10:29:19 +0530

    Airpods

---
+++ Airdopes.java
@@ -0,0 +1,17 @@
+class AirDopes{
+    public static void main(String[] ear){
+  System.out.println("main started");
+  String brandName= "Boat";
+  String color= "Black";
+  int price= 1029;
+  String Noice= "None";
+  System.out.println("the brand of the Airdoprs is" + brandName);
+  System.out.println("the color of the shoes is" + color);
+  System.out.println("the price of the shoe is" + price);
+  System.out.println("the Noice is" + Noice);
+  }
+  }
+  
+  
+  
+   
