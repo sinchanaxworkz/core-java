2b5e25c3e6d15ae4173ef2c1b5f62a2f2bccfa77
sinchanaxworkz <shettysinchana856@gmail.com>
2024-07-03 11:00:49 +0530

    Add files via upload

---
+++ Bluetooth.java
@@ -0,0 +1,17 @@
+class Bluetooth{
+    public static void main(String[] ear){
+  System.out.println("main started");
+  String brandName = "Boat";
+  String color = "Green";
+  int price = 2000;
+  String Noice = "None";
+  System.out.println("the brand name of the Bluetooth is" + brandName);
+  System.out.println("the color of the Bluetooth is" + color);
+  System.out.println("the price of the Bluetooth is" + price);
+  System.out.println("the Noice is" + Noice);
+  }
+  }
+  
+  
+  
+   
