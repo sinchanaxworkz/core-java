0b5b7f5d9e5dbeb08879778e82ee416d327a145b
sinchanxworkz <shettysinchana856@gmail.com>
2024-07-03 11:10:20 +0530

    Aded java

---
+++ division.java
@@ -0,0 +1,28 @@
+class division{
+public static void main (String[]div){
+System.out.println("Main started");
+	 div(23,33);
+	 div(33,23);
+	 
+     System.out.println("Main ended");
+	 }
+ public static void div (int n1,int n2){
+		System.out.println("div method started");
+		System.out.println(n1/n2);
+		System.out.println("div method started");
+		return ;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
