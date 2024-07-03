ceb4d5b1de3fb854915a8570558aef4f072ed89b
sinchanxworkz <shettysinchana856@gmail.com>
2024-07-03 10:20:19 +0530

    added hotel

---
+++ Hotel.java
@@ -0,0 +1,18 @@
+class Hotel {
+    public static void main(String[] args) {
+        System.out.println("Main Started");
+        String[] foodMenu = {"Idlivada","Idli Sambar","Rava Idli","Kanchipuram Idli","Podi Idli","Masala Vada","MeduVada","Thayir Vada","Masala Dosa","Plain Dosa","Rava Dosa","Mysore Dosa","Paper Dosa","Lemon Rice","Tomato Rice","Curd Rice","Biryani","South Indian Thali","Poori","Bonda","Sundal","Paniyaram","Murukku","Pallav","ChowChow Bath","Chapathi","OnionDosa"};
+        System.out.println("Welcome to our Hotel Udupi Grand:");
+        System.out.println("-.-.-.-.-.-..-.-.-.-.-..-.-.-");
+		//System.out.println("Hotel foodMenu are : " + foodMenu[0] + "\n" + foodMenu[1] + "\n" + foodMenu[2] + "\n" + foodMenu[3] + "\n" + foodMenu[4] + "\n" + foodMenu[5] + "\n" + foodMenu[6] + "\n" + foodMenu[7] + "\n" + foodMenu[8] + "\n" + foodMenu[9] + "\n" + foodMenu[10] + "\n" + foodMenu[11] + "\n" + foodMenu[12] + "\n" + foodMenu[13] + "\n" + foodMenu[14] + "\n" + foodMenu[15] + "\n" + foodMenu[16] + "\n" + foodMenu[17] + "\n" + foodMenu[18] + "\n" + foodMenu[19] + "\n" + foodMenu[20] + "\n" + foodMenu[21] + "\n" + foodMenu[22] + "\n" + foodMenu[23] + "\n" + foodMenu[24]);
+        for (String item : foodMenu) {
+            System.out.println(item);
+        }
+        System.out.println("Thank You For Visiting Our Hotel Udupi Grand");
+        System.out.println("Main Ended");
+    }
+}
+
+
+
+
