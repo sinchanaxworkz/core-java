3a41dd73524d594e4d42de179de6adef7c605310
sinchanxworkz <shettysinchana856@gmail.com>
2024-07-03 10:59:31 +0530

    Added java code

---
+++ Water.java
@@ -0,0 +1,9 @@
+class Water{
+    public static void main(String[] wa){
+	System.out.println("water is a coloeless ");
+	System.out.println("water is odourless");
+	System.out.println("water is very important for humanbeings");
+	System.out.println("water is also important for animals");
+	}
+
+}
